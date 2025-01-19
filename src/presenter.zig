f20807bfc3cf461aef6b227b1f9a1a1de358d0f2
knexator <daniel.hurtadoramirez314@gmail.com>
2025-01-19 14:46:39 +0100

    drawer

--- src/presenter.zig
+++ src/presenter.zig
@@ -104,13 +104,189 @@ pub const PlayerData = struct {
     }
 };
 
+pub const Vec2 = struct {
+    pub const Scalar = f32;
+
+    x: Scalar,
+    y: Scalar,
+
+    const Self = @This();
+
+    pub const zero = new(0, 0);
+    pub const one = new(1, 1);
+    pub const half = new(0.5, 0.5);
+    pub const e1 = new(1, 0);
+    pub const e2 = new(0, 1);
+
+    pub fn new(x: Scalar, y: Scalar) Self {
+        return .{ .x = x, .y = y };
+    }
+
+    pub fn add(a: Self, b: Self) Self {
+        return new(a.x + b.x, a.y + b.y);
+    }
+
+    pub fn sub(a: Self, b: Self) Self {
+        return new(a.x - b.x, a.y - b.y);
+    }
+
+    pub fn scale(v: Self, s: Scalar) Self {
+        return new(v.x * s, v.y * s);
+    }
+
+    pub fn perpCW(v: Self) Self {
+        return new(-v.y, v.x);
+    }
+
+    pub fn rotate(v: Self, turns: f32) Self {
+        const c = @cos(turns * std.math.tau);
+        const s = @sin(turns * std.math.tau);
+        return new(
+            v.x * c - v.y * s,
+            v.x * s + v.y * c,
+        );
+    }
+
+    test "rotate" {
+        try Vec2.expectApproxEqAbs(Vec2.e2, rotate(Vec2.e1, 0.25), 0.001);
+    }
+
+    pub fn normalized(v: Self) Self {
+        return v.scale(1 / v.mag());
+    }
+
+    pub fn mag(v: Self) Scalar {
+        return @sqrt(v.magSq());
+    }
+
+    pub fn magSq(v: Self) Scalar {
+        return dot(v, v);
+    }
+
+    pub fn dot(a: Self, b: Self) Scalar {
+        return a.x * b.x + a.y * b.y;
+    }
+
+    pub fn lerp(a: Self, b: Self, t: f32) Self {
+        return new(
+            std.math.lerp(a.x, b.x, t),
+            std.math.lerp(a.y, b.y, t),
+        );
+    }
+
+    pub fn expectApproxEqRel(expected: Vec2, actual: Vec2, tolerance: anytype) !void {
+        try std.testing.expectApproxEqRel(expected.x, actual.x, tolerance);
+        try std.testing.expectApproxEqRel(expected.y, actual.y, tolerance);
+    }
+
+    pub fn expectApproxEqAbs(expected: Vec2, actual: Vec2, tolerance: anytype) !void {
+        try std.testing.expectApproxEqAbs(expected.x, actual.x, tolerance);
+        try std.testing.expectApproxEqAbs(expected.y, actual.y, tolerance);
+    }
+};
+
+pub const Color = struct {
+    r: u8,
+    g: u8,
+    b: u8,
+
+    pub const white = new(255, 255, 255);
+    pub const black = new(0, 0, 0);
+
+    pub fn new(r: u8, g: u8, b: u8) Color {
+        return .{ .r = r, .g = g, .b = b };
+    }
+
+    pub fn gray(v: u8) Color {
+        return new(v, v, v);
+    }
+};
+
+pub const Point = struct {
+    pos: Vec2 = .zero,
+    scale: f32 = 1,
+    turns: f32 = 0,
+
+    pub fn lerp(a: Point, b: Point, t: f32) Point {
+        // TODO: properly handle rotation
+        return .{
+            .pos = Vec2.lerp(a.pos, b.pos, t),
+            .scale = std.math.lerp(a.scale, b.scale, t),
+            .turns = std.math.lerp(a.turns, b.turns, t),
+        };
+    }
+
+    pub fn applyToLocalPosition(parent: Point, local: Vec2) Vec2 {
+        return local.scale(parent.scale).rotate(parent.turns).add(parent.pos);
+    }
+
+    pub fn applyToLocalPoint(parent: Point, local: Point) Point {
+        return .{
+            .pos = parent.applyToLocalPosition(local.pos),
+            .scale = parent.scale * local.scale,
+            .turns = parent.turns + local.turns,
+        };
+    }
+
+    pub fn expectApproxEqRel(expected: Point, actual: Point, tolerance: anytype) !void {
+        try std.testing.expectApproxEqRel(expected.scale, actual.scale, tolerance);
+        try std.testing.expectApproxEqRel(expected.turns, actual.turns, tolerance);
+        try Vec2.expectApproxEqRel(expected.pos, actual.pos, tolerance);
+    }
+
+    pub fn expectApproxEqAbs(expected: Point, actual: Point, tolerance: anytype) !void {
+        try std.testing.expectApproxEqAbs(expected.scale, actual.scale, tolerance);
+        try std.testing.expectApproxEqAbs(expected.turns, actual.turns, tolerance);
+        try Vec2.expectApproxEqAbs(expected.pos, actual.pos, tolerance);
+    }
+
+    pub fn inverseApplyToLocalPoint(applied: Point, local: Point) Point {
+        const scale = applied.scale / local.scale;
+        const turns = applied.turns - local.turns;
+        return .{
+            .pos = applied.pos.sub(local.pos.scale(scale).rotate(turns)),
+            .scale = scale,
+            .turns = turns,
+        };
+    }
+
+    test "inverse apply" {
+        const parent: Point = .{ .pos = .zero, .scale = 2, .turns = 0.25 };
+        const local: Point = .{ .pos = .e1 };
+        const applied = parent.applyToLocalPoint(local);
+        try expectApproxEqAbs(.{ .pos = .new(0, 2), .scale = 2, .turns = 0.25 }, applied, 0.0001);
+        try expectApproxEqAbs(parent, applied.inverseApplyToLocalPoint(local), 0.0001);
+    }
+};
+
+pub const Camera = struct {
+    center: Vec2,
+    /// how many world units fit between the top and bottom of the camera view
+    height: f32,
+};
+
+pub const Drawer = struct {
+    clear: fn (color: Color) void,
+    drawAtomDebug: fn (camera: Camera, world_point: Point) void,
+    // drawAtomPatternDebug: fn (camera: Camera, world_point: Point) void,
+    // drawCable: fn (camera: Camera, world_from: Vec2, world_to: Vec2, world_scale: f32, offset: f32) void,
+};
+
 /// The full game, from loading screen to end credits
-pub fn Presenter(platform: Platform) type {
+pub fn Presenter(platform: Platform, drawer: Drawer) type {
     return struct {
         const Self = @This();
 
         mem: VeryPermamentGameStuff,
         persistence: PlayerData,
+
+        state: union(enum) {
+            intro: IntroSequence,
+        },
+
+        const IntroSequence = struct {
+            t: f32,
+        };
 
         pub fn init() !Self {
             const platform_alloc = platform.gpa;
@@ -134,10 +310,41 @@ pub fn Presenter(platform: Platform) type {
             try player_data.fnks.put(fnk.name, fnk.body);
             try platform.setPlayerData(player_data, &mem);
 
-            const result = Self{ .mem = mem, .persistence = player_data };
+            const result = Self{
+                .mem = mem,
+                .persistence = player_data,
+                .state = .{
+                    .intro = .{
+                        .t = 0.0,
+                    },
+                },
+            };
             // result.openLevel();
             // platform.showMenu()
             return result;
+        }
+
+        pub fn update(self: *Self, delta_seconds: f32) void {
+            switch (self.state) {
+                .intro => |*intro| {
+                    intro.t += delta_seconds;
+                },
+            }
+        }
+
+        pub fn draw(self: Self) void {
+            switch (self.state) {
+                .intro => |intro| {
+                    drawer.clear(Color.gray(128));
+                    drawer.drawAtomDebug(
+                        Camera{
+                            .center = .zero,
+                            .height = std.math.lerp(20, 4, std.math.clamp(intro.t / 2.0, 0.0, 1.0)),
+                        },
+                        Point{ .pos = .zero, .scale = 1, .turns = 0 },
+                    );
+                },
+            }
         }
     };
 }
--- src/web_platform.zig
+++ src/web_platform.zig
@@ -85,6 +85,48 @@ const js_better = struct {
             js.storage.setItem(key.ptr, key.len, value.ptr, value.len);
         }
     };
+
+    pub const canvas = struct {
+        pub fn getSize() Vec2 {
+            return Vec2.new(@floatFromInt(js.canvas.getWidth()), @floatFromInt(js.canvas.getHeight()));
+        }
+
+        pub fn setFillColor(c: Color) void {
+            js.canvas.setFillColor(c.r, c.g, c.b);
+        }
+
+        pub fn setStrokeColor(c: Color) void {
+            js.canvas.setStrokeColor(c.r, c.g, c.b);
+        }
+
+        pub fn moveTo(p: Vec2) void {
+            js.canvas.moveTo(p.x, p.y);
+        }
+
+        pub fn lineTo(p: Vec2) void {
+            js.canvas.lineTo(p.x, p.y);
+        }
+
+        pub fn clear(color: Color) void {
+            setFillColor(color);
+            const size = getSize();
+            js.canvas.fillRect(0, 0, size.x, size.y);
+        }
+
+        pub fn pathLoop(all_positions: []Vec2) void {
+            if (all_positions.len < 3) programmerError();
+            js.canvas.beginPath();
+            moveTo(all_positions[0]);
+            for (all_positions[1..]) |pos| {
+                lineTo(pos);
+            }
+            js.canvas.closePath();
+        }
+
+        pub fn circle(center: Vec2, radius: f32) void {
+            js.canvas.arc(center.x, center.y, radius, 0, std.math.tau, false);
+        }
+    };
 };
 
 const model = @import("main.zig");
@@ -111,14 +153,129 @@ const WebPlatform = struct {
     }
 };
 
+// TODO: delete this
+pub const CameraOld = struct {
+    // an object at [camera.topleft] will be drawn on the top left of the screen
+    // an object at [camera.topleft.addX(1) will be drawn 'asdf' pixels to the right of that
+
+    topleft: Vec2,
+    // how many pixels in a world unit
+    asdf: f32,
+
+    pub fn fromStuff(screen_side: f32, original_world: Point, target_screen_relative: Point) CameraOld {
+        const asdf = target_screen_relative.scale * screen_side / original_world.scale;
+        return .{
+            .topleft = original_world.pos.sub(
+                target_screen_relative.pos.scale(screen_side).scale(1 / asdf),
+            ),
+            .asdf = asdf,
+        };
+    }
+
+    test "fromStuff" {
+        {
+            const original = Point{ .pos = .new(3, 4), .scale = 1 };
+            const target_relative = Point{ .pos = Vec2.half, .scale = 0.1 };
+            const screen_side = 300;
+
+            const camera = fromStuff(screen_side, original, target_relative);
+            try Point.expectApproxEqRel(
+                .{ .pos = .new(150, 150), .scale = 30 },
+                camera.screenFromWorld(original),
+                0.000001,
+            );
+        }
+    }
+
+    pub fn screenFromWorld(this: CameraOld, world_point: Point) Point {
+        return .{
+            .pos = this.screenFromWorldPosition(world_point.pos),
+            .scale = this.screenFromWorldScale(world_point.scale),
+            .turns = world_point.turns,
+        };
+    }
+
+    pub fn screenFromWorldPosition(this: CameraOld, world_pos: Vec2) Vec2 {
+        return world_pos.sub(this.topleft).scale(this.asdf);
+    }
+
+    pub fn screenFromWorldScale(this: CameraOld, world_scale: f32) f32 {
+        return this.asdf * world_scale;
+    }
+
+    pub fn worldFromScreen(this: CameraOld, screen_point: Point) Point {
+        return .{
+            .pos = screen_point.pos.scale(1 / this.asdf).add(this.topleft),
+            .scale = screen_point.scale / this.asdf,
+            .turns = screen_point.turns,
+        };
+    }
+
+    test "basic camera" {
+        const camera = CameraOld{ .topleft = .new(2, 3), .asdf = 100 };
+        try std.testing.expectEqual(
+            Point{ .pos = Vec2.zero, .scale = 100 },
+            camera.screenFromWorld(
+                .{ .pos = .new(2, 3), .scale = 1 },
+            ),
+        );
+        try std.testing.expectEqual(
+            Point{ .pos = .new(100, 100), .scale = 50 },
+            camera.screenFromWorld(
+                .{ .pos = .new(3, 4), .scale = 0.5 },
+            ),
+        );
+    }
+};
+
+const Camera = presenter.Camera;
+const Point = presenter.Point;
+const Vec2 = presenter.Vec2;
+const Color = presenter.Color;
+const WebDrawer = struct {
+    fn screenFromWorld(camera: Camera, world_point: Point) Point {
+        const camera_old = CameraOld.fromStuff(
+            js_better.canvas.getSize().y,
+            .{ .pos = camera.center, .scale = 1 },
+            .{ .pos = .half, .scale = 1.0 / camera.height },
+        );
+        return camera_old.screenFromWorld(world_point);
+    }
+
+    pub fn drawAtomDebug(camera: Camera, world_point: Point) void {
+        const screen_point = screenFromWorld(camera, world_point);
+        const local_positions = [_]Vec2{
+            Vec2.new(-0.5, 0),
+            Vec2.new(0, 1),
+            Vec2.new(2, 1),
+            Vec2.new(2, -1),
+            Vec2.new(0, -1),
+        };
+        var screen_positions: [local_positions.len]Vec2 = undefined;
+        for (local_positions, 0..) |pos, i| {
+            screen_positions[i] = screen_point.applyToLocalPosition(pos);
+        }
+        js_better.canvas.pathLoop(&screen_positions);
+        js.canvas.setLineWidth(1);
+        js_better.canvas.setFillColor(Color.white);
+        js_better.canvas.setStrokeColor(Color.black);
+        js.canvas.fill();
+        js.canvas.stroke();
+    }
+};
+
 var gpa = std.heap.GeneralPurposeAllocator(.{}).init;
 const web_platform = presenter.Platform{
     .gpa = gpa.allocator(),
     .getPlayerData = WebPlatform.getPlayerData,
     .setPlayerData = WebPlatform.setPlayerData,
 };
+const web_drawer = presenter.Drawer{
+    .clear = js_better.canvas.clear,
+    .drawAtomDebug = WebDrawer.drawAtomDebug,
+};
 
-var game: presenter.Presenter(web_platform) = undefined;
+var game: presenter.Presenter(web_platform, web_drawer) = undefined;
 
 export fn init() void {
     game = @TypeOf(game).init() catch {
@@ -133,10 +290,12 @@ export fn keydown(code: KeyCode) void {
 }
 
 export fn frame(delta_seconds: f32) void {
-    _ = delta_seconds;
+    game.update(delta_seconds);
 }
 
-export fn draw() void {}
+export fn draw() void {
+    game.draw();
+}
 
 fn programmerError() noreturn {
     js.debug.logInt(666);
